c421b9cfabc7d267d2687beff18b2aaa855e5be1
Piotr Prus <prus.piotr@gmail.com>
2018-07-24 23:06:28 +0200

    initial commit for searchBar

--- app/src/main/java/com/example/godtfoodapp/ui/RecipeListActivity.kt
+++ app/src/main/java/com/example/godtfoodapp/ui/RecipeListActivity.kt
@@ -1,18 +1,25 @@
 package com.example.godtfoodapp.ui
 
+import android.app.SearchManager
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Context
+import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.SearchView
+import android.view.Menu
+import android.view.MenuItem
 import com.example.godtfoodapp.R
 import com.example.godtfoodapp.databinding.ActivityRecipeListBinding
 import com.example.godtfoodapp.di.ViewModelFactory
+import com.example.godtfoodapp.utils.toast
 
-class RecipeListActivity: AppCompatActivity() {
+class RecipeListActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRecipeListBinding
     private lateinit var viewModel: RecipeListViewModel
@@ -26,12 +33,13 @@ class RecipeListActivity: AppCompatActivity() {
 
         viewModel = ViewModelProviders.of(this, ViewModelFactory(this)).get(RecipeListViewModel::class.java)
         viewModel.errorMessage.observe(this, Observer { errorMessage ->
-            when(errorMessage) {
+            when (errorMessage) {
                 null -> hideError()
                 else -> showError(errorMessage)
             }
         })
         binding.viewModel = viewModel
+        handleIntent(intent)
     }
 
     private fun showError(errorMessage: Int) {
@@ -42,5 +50,36 @@ class RecipeListActivity: AppCompatActivity() {
 
     private fun hideError() {
         errorSnackBar?.dismiss()
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent?) {
+        if (Intent.ACTION_SEARCH == intent?.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY)
+            toast("I will search database for: $query")
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+        val searchView = menu?.findItem(R.id.search)?.actionView as SearchView
+        val searchManager: SearchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.setIconifiedByDefault(false)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.search -> {
+                onSearchRequested()
+                true
+            }
+            else -> false
+
+        }
     }
 }
